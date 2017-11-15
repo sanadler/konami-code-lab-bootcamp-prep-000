6eb6823457cb63a64ccb9166c6dbf3e3022d3130
Sarah Nadler <sarah.m.nadler@gmail.com>
2017-11-15 00:07:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', onKeyDownHandler(e));
 }
 
 let index = 0;

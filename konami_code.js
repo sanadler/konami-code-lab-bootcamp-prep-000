04cb8affdff5380b4a5774cc23f83fb272ae1525
Sarah Nadler <sarah.m.nadler@gmail.com>
2017-11-15 00:14:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,15 +12,14 @@ let index = 0;
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
 
+while(index<code.length){
   if (key === code[index]) {
     index++;
-
-    if (index === code.length) {
-      alert("Hurray!");
-
-      index = 0;
     }
-  } else {
+  }
+  alert("Horray!"); 
+}
+  else {
     index = 0;
   }
 }
